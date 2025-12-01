fd2b56b1b65fa87648235b24d8f05fcb42a95105
pmsoo <pmsoo4@naver.com>
2025-12-01 15:01:40 +0900

    test

---
+++ src/D1201/Test2.java
@@ -0,0 +1,12 @@
+package D1201;
+
+import java.io.*;
+import java.util.*;
+
+public class Test2 {
+
+    public static void main(String[] args) throws Exception {
+
+        // TODO: Solve
+    }
+}
