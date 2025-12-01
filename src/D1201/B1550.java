727c1d9b87682b15e02f1cfd5a87b5fc0c497276
pmsoo <pmsoo4@naver.com>
2025-12-01 15:59:30 +0900

    B1550

---
+++ src/D1201/B1550.java
@@ -0,0 +1,18 @@
+package D1201;
+
+import java.io.*;
+import java.util.*;
+
+public class B1550 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        int c = Integer.parseInt(a, 16);
+        System.out.println(c);
+
+        // TODO: Solve
+    }
+}
