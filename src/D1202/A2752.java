9ece0829dc0cb83b455bee0f6ee22511dc8a40cf
pmsoo <pmsoo4@naver.com>
2025-12-02 23:40:53 +0900

    A2752

---
+++ src/D1202/A2752.java
@@ -0,0 +1,35 @@
+package D1202;
+
+import java.io.*;
+import java.util.*;
+
+public class A2752 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int A[] = new int[3];
+
+        for(int i = 0; i<3; i++){
+            A[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<3; i++){
+            for(int j = i; j<3; j++){
+                if(A[i]>A[j]){
+                    int temp = A[i];
+                    A[i] = A[j];
+                    A[j] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i<3; i++){
+            System.out.print(A[i]+" ");
+
+        }
+
+
+        // TODO: Solve
+    }
+}
