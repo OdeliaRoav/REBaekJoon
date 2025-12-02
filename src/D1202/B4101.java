08fca6ded4e045307ae714786558ea20cc914a36
pmsoo <pmsoo4@naver.com>
2025-12-03 00:09:54 +0900

    B4101

---
+++ src/D1202/B4101.java
@@ -0,0 +1,32 @@
+package D1202;
+
+import java.io.*;
+import java.util.*;
+
+public class B4101 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            else{
+                if(a>b) {
+                    System.out.println("Yes");
+                }
+                else{
+                    System.out.println("No");
+                }
+            }
+
+        }
+
+
+
+        // TODO: Solve
+    }
+}
