69050bad362700c69ec49a11cdb065773c27b23c
pmsoo <pmsoo4@naver.com>
2025-12-02 23:50:57 +0900

    A1550

---
+++ src/D1202/A1550.java
@@ -0,0 +1,20 @@
+package D1202;
+
+import java.util.*;
+
+public class A1550 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+
+        int A = Integer.parseInt(a, 16);
+
+
+        System.out.print(A);
+
+
+        // TODO: Solve
+    }
+}
