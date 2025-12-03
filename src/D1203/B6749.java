4bc55704bbce0bb62745feed8ea8f66c389392bc
pmsoo <pmsoo4@naver.com>
2025-12-03 13:55:59 +0900

    B6749

---
+++ src/D1203/B6749.java
@@ -0,0 +1,24 @@
+package D1203;
+
+import java.io.*;
+import java.util.*;
+
+public class B6749 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int f_num = sc.nextInt();
+        int m_num = sc.nextInt();
+
+        int l_num = m_num*2 - f_num;
+
+        System.out.print(l_num);
+
+        //m_num = (l_num+f_num)/2
+        //l_num = m_num*2 - f_num
+
+
+
+    }
+}
