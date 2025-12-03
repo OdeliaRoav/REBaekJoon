c1ee6bba2ee77d5a02c8b58ea8cd0ab2b48b12f6
pmsoo <pmsoo4@naver.com>
2025-12-03 13:43:14 +0900

    B5338

---
+++ src/D1203/B5338.java
@@ -0,0 +1,16 @@
+package D1203;
+
+import java.io.*;
+import java.util.*;
+
+public class B5338 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+
+    }
+}
