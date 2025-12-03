a8b45cf06b01f422bd8ae54c654e68c23cad8c9d
pmsoo <pmsoo4@naver.com>
2025-12-03 16:31:27 +0900

    B7287

---
+++ src/D1203/B7287.java
@@ -0,0 +1,13 @@
+package D1203;
+
+import java.io.*;
+import java.util.*;
+
+public class B7287 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("127");
+        System.out.print("pmsoo4");
+
+    }
+}
