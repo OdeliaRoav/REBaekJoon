108d2f82819d3c4ee09b84e38d49d4b3cd183eaa
pmsoo <pmsoo4@naver.com>
2025-12-03 13:41:34 +0900

    B5337

---
+++ src/D1203/B5337.java
@@ -0,0 +1,14 @@
+package D1203;
+
+import java.io.*;
+import java.util.*;
+
+public class B5337 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
