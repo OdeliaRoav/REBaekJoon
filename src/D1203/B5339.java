31402c9724c8dcc989585286eba943a9baec581e
pmsoo <pmsoo4@naver.com>
2025-12-03 13:46:09 +0900

    B5339

---
+++ src/D1203/B5339.java
@@ -0,0 +1,21 @@
+package D1203;
+
+import java.io.*;
+import java.util.*;
+
+public class B5339 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+
+    }
+}
