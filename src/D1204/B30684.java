225ab1bfc96d7230f13228036772a1959659ffb2
pmsoo <pmsoo4@naver.com>
2025-12-04 12:56:05 +0900

    B30684

---
+++ src/D1204/B30684.java
@@ -0,0 +1,24 @@
+package D1204;
+
+import java.io.*;
+import java.util.*;
+
+public class B30684 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String [] arr = new String[N];
+
+        for(int i = 0; i<N; i++) {
+            arr[i] = sc.next();
+
+        }
+
+
+
+
+
+    }
+}
