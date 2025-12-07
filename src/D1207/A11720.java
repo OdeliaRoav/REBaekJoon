f1bdaa72e8a0bc3736da462294e24a1dfe8ceb09
pmsoo <pmsoo4@naver.com>
2025-12-08 01:01:41 +0900

    A11720

--- src/D1207/A11720.java
+++ src/D1207/A11720.java
@@ -9,16 +9,18 @@ public class A11720 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-
-        String N = sc.next();
-
+        int N = sc.nextInt();
+        String s = sc.next();
         int sum = 0;
+        char C[] = new char[N];
 
-        for(int i = 0; i<num; i++){
-            sum = sum + Integer.parseInt(String.valueOf(N.charAt(i)));
+        for(int i = 0; i< N; i++){
+            C[i] = s.charAt(i);
         }
 
+        for(int i = 0; i<N; i++){
+            sum = sum + Integer.parseInt(String.valueOf(C[i]));
+        }
 
         System.out.println(sum);
     }
