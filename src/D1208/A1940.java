064402d235f779f7ee575efcc02628ac7ca0f4c5
pmsoo <pmsoo4@naver.com>
2025-12-08 18:31:45 +0900

    A1940

---
+++ src/D1208/A1940.java
@@ -0,0 +1,46 @@
+package D1208;
+
+import java.io.*;
+import java.util.*;
+
+public class A1940 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N  = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+
+        int A[] = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int i = 0;
+        int j = N-1;
+        int count = 0;
+
+        while(i<j){
+            int sum = A[i] + A[j];
+            if(sum==M){
+                count++;
+                i++;
+                j--;
+            }
+            else if(sum<M){
+                i++;
+            }
+            else if(sum>M){
+                j--;
+            }
+
+        }
+        System.out.println(count);
+    }
+}
