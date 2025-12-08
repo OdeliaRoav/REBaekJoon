ceacf76fbb92a30f47ef7169b0efd2f09642d7b6
pmsoo <pmsoo4@naver.com>
2025-12-08 17:55:28 +0900

    B2018

---
+++ src/D1208/B2018.java
@@ -0,0 +1,39 @@
+package D1208;
+
+import java.io.*;
+import java.util.*;
+
+public class B2018 {
+
+    public static void main(String[] args) throws Exception {
+        //투 포인터
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int start = 1;
+        int end = 1;
+        int count = 1;
+        int sum = 1;
+
+        while(end != N){
+            if(sum == N){
+                count ++;
+                end++;
+                sum = sum + end;
+            }
+            else if(sum<N){
+                end++;
+                sum = sum + end;
+            }
+            else if(sum>N){
+                sum = sum -start;
+                start++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
