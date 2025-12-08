359a126baf8b7cf5c6a4987cec3784ddbf57c4c5
pmsoo <pmsoo4@naver.com>
2025-12-09 01:53:54 +0900

    A2018

---
+++ src/D1209/A2018.java
@@ -0,0 +1,37 @@
+package D1209;
+
+import java.io.*;
+import java.util.*;
+
+public class A2018 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int start = 1;
+        int end = 1;
+        int count = 1;
+        int sum = 1;
+
+        while(end != N){
+            if(sum == N){
+                count ++;
+                end++;
+                sum = sum +end;
+            }
+            else if (sum > N){
+                sum = sum - start;
+                start++;
+            }
+            else if(sum < N){
+                end++;
+                sum = sum + end;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
