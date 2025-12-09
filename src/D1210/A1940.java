c4866a29da586a54b5ee32d9395341a7bdc819f5
pmsoo <pmsoo4@naver.com>
2025-12-10 01:53:17 +0900

    A1940

--- src/D1210/A1940.java
+++ src/D1210/A1940.java
@@ -13,7 +13,33 @@ public class A1940 {
         st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
 
+        int A[] = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
 
+        Arrays.sort(A);
+
+        int i = 0;
+        int j = N-1;
+        int count = 0;
+
+        while(i<j){
+            if(A[i] + A[j]== M){
+                count++;
+                i++;
+                j--;
+            }
+            else if(A[i] + A[j]>M){
+                j--;
+            }
+            else if(A[i]+A[j]<M){
+                i++;
+            }
+
+        }
+        System.out.println(count);
 
 
     }
