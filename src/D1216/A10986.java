998b785dc4db5447d7d11e09e9c7fa596c3c4f22
pmsoo <pmsoo4@naver.com>
2025-12-17 01:50:56 +0900

    A10986

--- src/D1216/A10986.java
+++ src/D1216/A10986.java
@@ -6,7 +6,36 @@ import java.util.*;
 public class A10986 {
 
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
+        long A[] = new long[N];
+        long C[] = new long[M];
+
+        A[0] = sc.nextInt();
+        for(int i = 1; i < N; i++){
+            A[i] = A[i-1] + sc.nextInt();
+        }
+
+        long count = 0;
+
+        for(int i = 0; i < N; i++){
+            int contrainter = (int)(A[i]%M);
+
+            if(contrainter == 0){
+                count++;
+            }
+            C[contrainter]++;
+        }
+
+        for(int i = 0; i < M; i++){
+            if(C[i]>1){
+                count = count + (C[i]*(C[i]-1)/2);
+            }
+        }
+
+        System.out.println(count);
     }
 }
