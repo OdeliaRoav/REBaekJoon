7fbdef640a158e9d25d248e52bb27033b69cf903
pmsoo <pmsoo4@naver.com>
2025-12-19 01:43:45 +0900

    B2164

---
+++ src/D1219/B2164.java
@@ -0,0 +1,26 @@
+package D1219;
+
+import java.io.*;
+import java.util.*;
+
+public class B2164 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Queue<Integer> myQueue = new LinkedList<>();
+
+        for(int i = 1; i <= N; i++){
+            myQueue.add(i);
+        }
+
+        while(myQueue.size() > 1){
+            myQueue.poll();
+            myQueue.add(myQueue.poll());
+        }
+
+        System.out.println(myQueue.poll());
+    }
+}
