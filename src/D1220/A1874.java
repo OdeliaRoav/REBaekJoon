a6fd46e11164421a0b52ada9eed63e8875aa0a99
pmsoo <pmsoo4@naver.com>
2025-12-20 01:46:59 +0900

    A1874

--- src/D1220/A1874.java
+++ src/D1220/A1874.java
@@ -6,7 +6,39 @@ import java.util.*;
 public class A1874 {
 
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
 
+        Stack<Integer> stack = new Stack<>();
 
+        int count = 1;
+        boolean flag = true;
+        for(int i = 0; i < N; i++){
+            int now = Integer.parseInt(br.readLine());
+            while(count <= now){
+                stack.push(count++);
+                sb.append("+\n");
+            }
+
+            if(!stack.isEmpty()&&stack.peek() == now){
+                stack.pop();
+                sb.append("-\n");
+            }
+            else{
+                flag = false;
+            }
+
+        }
+
+        if(flag){
+            bw.write(sb.toString());
+        }
+        else{
+            System.out.println("NO");
+        }
+        bw.flush();
+        bw.close();
     }
 }
