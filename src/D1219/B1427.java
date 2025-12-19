31f8f3c83f976c71ade60f4acca4f04675bd3888
pmsoo <pmsoo4@naver.com>
2025-12-20 00:23:37 +0900

    B1427

--- src/D1219/B1427.java
+++ src/D1219/B1427.java
@@ -6,7 +6,31 @@ import java.util.*;
 public class B1427 {
 
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String s = br.readLine();
+        int A[] = new int [s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+            A[i] = Integer.parseInt(s.charAt(i) + "");
+        }
+
+        for(int i = 0; i<s.length(); i++){
+            int max = i;
+            for(int j = i+1; j<s.length(); j++){
+                if(A[j]>A[max]){
+                    max = j;
+                }
+                if(A[i]<A[max]){
+                    int temp = A[i];
+                    A[i] = A[max];
+                    A[max] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<s.length(); i++){
+            System.out.print(A[i]);
+        }
 
     }
 }
