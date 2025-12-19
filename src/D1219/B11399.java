ee7fb4f66c20c4e4f2c4977c8ef82ef1c53b5d50
pmsoo <pmsoo4@naver.com>
2025-12-20 00:26:52 +0900

    B11399

--- src/D1219/B11399.java
+++ src/D1219/B11399.java
@@ -6,7 +6,27 @@ import java.util.*;
 public class B11399 {
 
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+        int S[] = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        S[0] = A[0];
+        for(int i = 1; i < N; i++){
+            S[i] = S[i-1] + A[i];
+        }
+        int sum = 0;
+        for(int i = 0; i < N; i++){
+            sum = sum + S[i];
+        }
+
+        System.out.println(sum);
     }
 }
