e7f061112d2dfd648b4fb9dfafdebc26d43afd46
pmsoo <pmsoo4@naver.com>
2025-12-27 05:00:44 +0900

    A30684

--- src/D1226/A30684.java
+++ src/D1226/A30684.java
@@ -6,7 +6,24 @@ import java.util.*;
 public class A30684 {
 
     public static void main(String[] args) throws Exception {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st = new StringTokenizer(br.readLine());
 
+         int N = Integer.parseInt(st.nextToken());
 
+         String name = "ZZZ";
+
+         for(int i = 0; i < N; i++){
+             String member = br.readLine();
+
+             if(member.length() != 3){
+                 continue;
+             }
+             if(member.compareTo(name) < 0){
+                 name = member;
+             }
+         }
+
+         System.out.println(name);
     }
 }
