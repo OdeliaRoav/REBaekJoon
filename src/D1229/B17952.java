ab048d646f09f0d730f45c602662a1569b3be319
pmsoo <pmsoo4@naver.com>
2025-12-29 04:04:23 +0900

    B17952

---
+++ src/D1229/B17952.java
@@ -0,0 +1,24 @@
+package D1229;
+
+import java.io.*;
+import java.util.*;
+
+public class B17952 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+
+        Stack<Integer> myStack = new Stack<>();
+
+        for(int i = 0; i < N; i++){
+            int count =
+        }
+
+
+    }
+}
