b0a767850d57fee1bb8d5a5a4b4f520f659e1b0b
pmsoo <pmsoo4@naver.com>
2026-02-03 04:44:00 +0900

    B15650

---
+++ src/D0203/B15650.java
@@ -0,0 +1,46 @@
+package D0203;
+
+import java.io.*;
+import java.util.*;
+
+public class B15650 {
+    static int N, M;
+    static int A[];
+    static boolean V[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = new int[N];
+        V = new boolean[N];
+
+        backtracking(0, 0);
+
+
+    }
+
+    static void backtracking(int length, int start){
+        if(length == M ){
+            printArray();
+            return;
+        }
+        for(int i = start; i < N; i++){
+            if(V[i] == false){
+                V[i] = true;
+                A[length] = i;
+                backtracking(length + 1, i + 1);
+                V[i] = false;
+            }
+        }
+    }
+    static void printArray(){
+        for(int i = 0; i < M; i++){
+            System.out.print(A[i] + 1 + " ");
+        }
+        System.out.println();
+    }
+}
